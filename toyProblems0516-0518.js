a732fbd2f782f00de9c8dbb85b0d51b80cf0b1c7
sxcw <yuan329419@gmail.com>
2016-05-17 10:06:18 -0500

    Add tribonacii, divisors, titlecase

--- toyProblems0516-0518.js
+++ toyProblems0516-0518.js
@@ -81,13 +81,114 @@ function toWeirdCase(string){
     return word.split('').map(function(letter, index){
       return index % 2 == 0 ? letter.toUpperCase() : letter.toLowerCase()
     }).join('');
-
-
-
-
-
-
-
-    
   }).join(' ');
 }
+
+
+
+// *****=========================================
+// *****=========================================
+// *****=========================================
+// *****=========================================
+// *****=========================================
+// http://www.codewars.com/kata/tribonacci-sequence
+
+function tribonacci(signature,n){
+	var array = signature;
+	if(n < 4 && n >= 0){
+		return array.slice(0,n);
+	}
+	function helper(array,n) {
+		if(array[n] !== undefined) {
+			return array[n];
+		}
+		if (n <= 2){
+  			return array[n];
+  		} else {
+  			return array[n] = helper(array,n-1) + helper(array,n-2) + helper(array,n-3);
+  		}
+	}
+	helper(array,n-1);
+	return array;  
+}
+tribonacci([1,1,1],1)
+tribonacci([1,1,1],10) // [1,1,1,3,5,9,17,31,57,105]
+tribonacci([0,0,1],10) // [0,0,1,1,2,4,7,13,24,44]
+ //[1]
+tribonacci([300,200,100],0) //[]
+tribonacci([1,2,3],10) // [1,2,3,6,11,20,37,68,125,230])
+
+tribonacci([0.5,0.5,0.5],30) // [0.5,0.5,0.5,1.5,2.5,4.5,8.5,15.5,28.5,52.5,96.5,177.5,326.5,600.5,1104.5,2031.5,3736.5,6872.5,12640.5,23249.5,42762.5,78652.5,144664.5,266079.5,489396.5,900140.5,1655616.5,3045153.5,5600910.5,10301680.5])
+
+// other solution, efficient
+function tribonacci(signature,n){  
+  for (var i = 0; i < n-3; i++) { // iterate n times
+    signature.push(signature[i] + signature[i+1] + signature[i+2]); // add last 3 array items and push to trib
+  }
+  return signature.slice(0, n); //return trib - length of n
+}
+
+
+
+
+// *****=========================================
+// *****=========================================
+// *****=========================================
+// *****=========================================
+// *****=========================================
+// http://www.codewars.com/kata/find-the-divisors
+
+function divisors(integer) {
+  var result = [];
+  for(var i = 2; i <= Math.floor(integer/2) ; i++){
+  	if(integer % i ===0){
+  		result.push(i);
+  	}
+  }
+  if(result.length === 0){
+  	return integer + " is prime"
+  } else {
+  	return result;
+  }
+};
+
+divisors(12) // [2,3,4,6]
+divisors(13) // "13 is prime"
+
+
+
+
+
+// *****=========================================
+// *****=========================================
+// *****=========================================
+// *****=========================================
+// *****=========================================
+// http://www.codewars.com/kata/title-case  
+
+function titleCase(title, minorWords) {
+  if(title === undefined || title === "") {
+  	return "";
+  } else {
+  	return title.toLowerCase().split(' ').map(function(word,index){ 	
+	  	if (minorWords !== undefined && index > 0 && minorWords.toLowerCase().split(' ').indexOf(word) > -1){
+	  		word = word.toLowerCase();
+	  	} else {
+	  		word = word.substring(0,1).toUpperCase()+ word.substring(1).toLowerCase();
+	  	}
+	  	return word;
+  	}).join(' ');
+  } 
+}
+
+titleCase("a clash of KINGS","a an the of");  // A Clash of Kings
+titleCase('THE WIND IN THE WILLOWS','The In') // The Wind in the Willows
+
+
+
+
+
+
+
+
+
